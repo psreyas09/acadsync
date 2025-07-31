b74a717c1e94e47fe5e1e002a334da5ef7a755b7
Sreyas P <psreyas09@gmail.com>
2025-07-31 21:17:03 +0530

    directory

--- test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    // This is a simple Java class named Test.
-
-public static void main(String[] args) {
-    // This is a simple Java program that prints "Hello, World!" to the console.
-    System.out.println("Hello");
-}
-
-}
