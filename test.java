985c82ae370627e70f33217a23ed673a127be13a
Sreyas P <psreyas09@gmail.com>
2025-07-31 20:46:05 +0530

    test

--- test.java
+++ test.java
@@ -1,4 +1,8 @@
+public class Test {
+    // This is a simple Java class named Test.
+
 public static void main(String[] args) {
     // This is a simple Java program that prints "Hello, World!" to the console.
     System.out.println("Hello, World!");
+}
 }
